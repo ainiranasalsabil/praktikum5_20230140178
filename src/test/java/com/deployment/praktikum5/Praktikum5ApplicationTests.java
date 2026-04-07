85fc5aac6cd89f102a910437e5e6ec7d5933e2ef
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-04-07 11:09:20 +0700

    Initial commit

---
+++ src/test/java/com/deployment/praktikum5/Praktikum5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deployment.praktikum5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
